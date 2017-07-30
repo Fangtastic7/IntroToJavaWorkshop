7bdcd5267f319ee11756df653f4670958826fdf0
Garrett-Thomas <league@WTS10.attlocal.net>
2017-07-30 15:26:59 -0700

    hi

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
+		Robot daniel=new Robot();
+		daniel.setSpeed(10);
+		daniel.sparkle();
+		daniel.penDown();
+		daniel.setPen
+		for(int i=0; i<4; i++){
+			daniel.move(100);
+			daniel.turn(90);
+		}
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,31 +5,36 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+		Robot bot = new Robot();
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		 // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+			bot.setSpeed(10);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+			int x=50;
 		// 7. Do the following (up to step 10) 60 times
-
+			for(int i=0; i<60;i++){
+				
 			// 9. Change the color of the pen to a random color
-	
+				bot.setRandomPenColor();
 			// 8. Increase the length of the side by 10 pixels
-	
+				x=x+10;
 			// 5. call your drawTriangle() method using your length variable
-	
+				drawTriangle(x);
 			// 10. Turn the tortoise 6 degrees to the right
-
+				bot.turn(6);
+			}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
-		
-		
+		bot.penDown();
+		for(int i=0; i<3; i++){
+		bot.turn(360/3);
+		bot.move(length); 
+		}
 		
 	}
 
